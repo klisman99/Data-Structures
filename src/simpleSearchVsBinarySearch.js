7db365d77f1d61f67e61db33778b4c0229c03ac3
Klisman Falcão <klismanflu@gmail.com>
2023-10-22 21:26:49 -0300

    Styling

--- src/simpleSearchVsBinarySearch.js
+++ src/simpleSearchVsBinarySearch.js
@@ -4,50 +4,50 @@ const bigArray = Array.from(new Array(ARRAY_SIZE).keys());
 const lastArrayItem = bigArray.at(-1);
 
 function simpleSearch(arr, num) {
-  let findedIndex = undefined;
-  let countOperations = 0;
+	let findedIndex = undefined;
+	let countOperations = 0;
 
-  for (let i = 0; i <= arr.length; i++) {
-    countOperations++;
+	for (let i = 0; i <= arr.length; i++) {
+		countOperations++;
 
-    if (arr[i] === num) {
-      findedIndex = i;
-      break;
-    }
-  }
+		if (arr[i] === num) {
+			findedIndex = i;
+			break;
+		}
+	}
 
-  console.log("Operations: ", countOperations);
+	console.log("Operations: ", countOperations);
 
-  return findedIndex;
+	return findedIndex;
 }
 
 function binarySearch(arr, num) {
-  let findedIndex = undefined;
-  let countOperations = 0;
-  
-  let lowestElm = 0;
-  let highestElm = arr.length - 1;
-  
-  while (lowestElm <= highestElm) {
-    countOperations++;
-    
-    const middleElmIndex = parseInt((lowestElm + highestElm) / 2);
-    
-    if (num === arr[middleElmIndex]) {
-      findedIndex = middleElmIndex;
-      break;
-    }
-    
-    if (arr[middleElmIndex] < num) {
-      lowestElm = middleElmIndex + 1;
-    } else {
-      highestElm = middleElmIndex - 1;
-    }
-  }
-  
-  console.log("Operations: ", countOperations);
-  
-  return findedIndex;
+	let findedIndex = undefined;
+	let countOperations = 0;
+
+	let lowestElm = 0;
+	let highestElm = arr.length - 1;
+
+	while (lowestElm <= highestElm) {
+		countOperations++;
+
+		const middleElmIndex = parseInt((lowestElm + highestElm) / 2);
+
+		if (num === arr[middleElmIndex]) {
+			findedIndex = middleElmIndex;
+			break;
+		}
+
+		if (arr[middleElmIndex] < num) {
+			lowestElm = middleElmIndex + 1;
+		} else {
+			highestElm = middleElmIndex - 1;
+		}
+	}
+
+	console.log("Operations: ", countOperations);
+
+	return findedIndex;
 }
 
 console.log("Searching for: ", lastArrayItem);
